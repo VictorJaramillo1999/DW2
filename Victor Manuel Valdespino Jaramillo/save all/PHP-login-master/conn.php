d2bd6c0cebb8e8c81b6993f8114b9a185f75fddd
Victor Manuel Valdespino Jaramillo <55413702+VictorJaramillo1999@users.noreply.github.com>
2020-06-17 18:53:46 -0500

    carpeta
    xdxdxd

---
+++ Victor Manuel Valdespino Jaramillo/save all/PHP-login-master/conn.php
@@ -0,0 +1,12 @@
+<?php
+// Connection variables
+$dbhost	= "localhost";	   // localhost or IP
+$dbuser	= "root";		  // database username
+$dbpass	= "";		     // database password
+$dbname	= "phplogin";    // database name
+
+// $mysqli = new mysqli("localhost","root","","phplogin");
+
+//if(mysqli_connect_errno()){echo 'Conexion fallida: ', mysql_connect_error();}
+
+?>
